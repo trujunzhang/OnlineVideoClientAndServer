511fa3fc24205415ba20ad8efea95a1fac0ad889
wanghaogithub720 <wanghaomspace@gmail.com>
2015-01-17 16:47:34 +0800

    dev .

--- dependencies/server/media_logical_layer/Pod/Classes/GeneratethumbnailTask/GenerateThumbnailTask.m
+++ dependencies/server/media_logical_layer/Pod/Classes/GeneratethumbnailTask/GenerateThumbnailTask.m
@@ -11,44 +11,45 @@
 }
 
 + (void)executeGenerateThumbnailTaskFrom:(NSString *)fileAbstractPath to:(NSString *)destinateFilePath {
-   NSString * format = [NSString stringWithFormat:
-    @"ffmpeg -i \"%@\" -deinterlace -an -ss 1 -t 00:00:10 -s 320x180 -r 1 -y -vcodec mjpeg -f mjpeg \"%@\"",
-    fileAbstractPath,
-    destinateFilePath];
-   [GenerateThumbnailTask runCommand:format];
+    NSString *format = [NSString stringWithFormat:
+            @"ffmpeg -i \"%@\" -deinterlace -an -ss 1 -t %@ -s 320x180 -r 1 -y -vcodec mjpeg -f mjpeg \"%@\"",
+            fileAbstractPath,
+            @"00:00:22",
+            destinateFilePath];
+    [GenerateThumbnailTask runCommand:format];
 }
 
 
 + (NSString *)runCommand:(NSString *)commandToRun {
-   NSTask * task;
-   task = [[NSTask alloc] init];
-   [task setLaunchPath:@"/bin/sh"];
+    NSTask *task;
+    task = [[NSTask alloc] init];
+    [task setLaunchPath:@"/bin/sh"];
 
-   NSArray * arguments = [NSArray arrayWithObjects:
-    @"-c",
-    [NSString stringWithFormat:@"%@", commandToRun],
-     nil];
-   [task setArguments:arguments];
+    NSArray *arguments = [NSArray arrayWithObjects:
+            @"-c",
+            [NSString stringWithFormat:@"%@", commandToRun],
+                    nil];
+    [task setArguments:arguments];
 
-   NSPipe * pipe;
-   pipe = [NSPipe pipe];
-   [task setStandardOutput:pipe];
+    NSPipe *pipe;
+    pipe = [NSPipe pipe];
+    [task setStandardOutput:pipe];
 
-   NSFileHandle * file;
-   file = [pipe fileHandleForReading];
+    NSFileHandle *file;
+    file = [pipe fileHandleForReading];
 
-   [task launch];
+    [task launch];
 
-   NSData * data;
-   data = [file readDataToEndOfFile];
+    NSData *data;
+    data = [file readDataToEndOfFile];
 
-   NSString * output;
-   output = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    NSString *output;
+    output = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
 
-   NSLog(@"run command: %@", commandToRun);
-   NSLog(@"output = %@", output);
+    NSLog(@"run command: %@", commandToRun);
+    NSLog(@"output = %@", output);
 
-   return output;
+    return output;
 }
 
 
