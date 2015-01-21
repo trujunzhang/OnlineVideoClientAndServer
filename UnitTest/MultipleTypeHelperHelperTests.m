5143fb765f86f63c06ca8a2d01b6fb8921467a67
wanghaogithub720 <wanghaomspace@gmail.com>
2015-01-21 13:30:39 +0800

    dev .

---
+++ UnitTest/MultipleTypeHelperHelperTests.m
@@ -0,0 +1,37 @@
+//
+//  MultipleTypeHelperHelperTests.m
+//  OnlineVideoClient
+//
+//  Created by djzhang on 1/2/15.
+//  Copyright (c) 2015 djzhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import "Expecta.h"
+
+@interface MultipleTypeHelperHelperTests : XCTestCase
+
+@end
+
+
+@implementation MultipleTypeHelperHelperTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+
+- (void)testExample {
+}
+
+
+@end
