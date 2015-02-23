e68d428d83306c42bf825bf493614d61bc0f6eef
wanghaogithub720 <wanghaomspace@gmail.com>
2015-02-23 13:14:46 +0800

    dev .

---
+++ dependencies/ipad/Youtube_ChannelPage_Controller/Pod/Classes/TabbarPage/TabbarPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  TabbarPageViewController.h
+//  IOSTemplate
+//
+//  Created by djzhang on 2/23/15.
+//  Copyright (c) 2015 djzhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabbarPageViewController : UIViewController
+
+@end
