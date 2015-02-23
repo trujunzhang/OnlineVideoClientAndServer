5d95c88746fb6632a3a85d9d534856667ff136f4
wanghaogithub720 <wanghaomspace@gmail.com>
2015-02-23 12:54:01 +0800

    dev .

--- dependencies/layers/Business-Logic-Layer/Pod/Classes/AppDelegate/OnlineAppDelegate.m
+++ dependencies/layers/Business-Logic-Layer/Pod/Classes/AppDelegate/OnlineAppDelegate.m
@@ -39,13 +39,6 @@
 
 @implementation OnlineAppDelegate
 
-- (BOOL)application123:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    self.window.rootViewController = [self setupRevealViewController];
-    [self.window makeKeyAndVisible];
-    return YES;
-}
-
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
